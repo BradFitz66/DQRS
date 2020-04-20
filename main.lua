8691dc9d83dd5279450160779ed9cdb32d55b690
Bradley Bath <badfitz66@gmail.com>
2020-04-20 03:35:18 +0100

    Implemented artal for rendering PSD files Added TLFres for easy resolution scaling Implemented basic camera using HUMP.camera
    ToDo:
    Collision using hardon (generate collider for collisionmap layer of psd)

--- main.lua
+++ main.lua
@@ -6,22 +6,27 @@ vector=nil
 blendtree=nil
 entity=nil
 camera=nil
+artal=nil
 --
+local tlfres=require("Resources.lib.TLfres")
 local player;
 local is_playing=true;
 local do_step=false;
 local debugInput;
+local map;
 --Use this for initialization
 function love.load()
+	print(tlfres)
 	love.graphics.setDefaultFilter("nearest","nearest",3)
 	anim8=require("Resources.lib.anim8")
 	Input=require("Resources.lib.Input")
 	vector=require("Resources.lib.HUMP.vector")
 	blendtree=require("Resources.lib.blendtree")
 	entity=require("Resources.scripts.Entity")
+	artal=require("Resources.lib.artal")
 	player=require("Resources.scripts.Player").load()
 	player:loadTree("idle")
-	camera=require("Resources.lib.HUMP.camera")(player.position.x,player.position.y,2)
+	camera=require("Resources.lib.HUMP.camera")(player.position.x,player.position.y,1)
 	debugInput=Input.new {
 		
 		controls = {
@@ -32,11 +37,12 @@ function love.load()
 		},
 		joystick = love.joystick.getJoysticks()[1],
 	}
+	map=artal.newPSD("Resources/graphics/PSD/TankBottomFloor.psd")
 end
 
 --Use this for drawing objects
 function love.draw()
-	
+	tlfres.beginRendering(256, 192)
 	love.graphics.setBackgroundColor(64/255,234/255,248/255)
 	if(debug) then
 		love.graphics.setColor(0,0,0)
@@ -47,8 +53,23 @@ function love.draw()
 		love.graphics.setColor(255,255,255)
 	end
 	camera:attach()
+	for i=1,#map do
+		if(map[i].name~="CollisionMap") then
+			love.graphics.draw(
+			map[i].image,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			map[i].ox,
+			map[i].oy
+			)
+		end
+	end
 	player:draw()
 	camera:detach()
+	tlfres.endRendering()
 end
 
 
@@ -57,7 +78,7 @@ function love.update(dt)
 	if is_playing or do_step then
 		--dostep=false;
 		player:update(dt)
-		camera:position(player.position.x,player.position.y)
+		camera:lockPosition(player.position.x, player.position.y)
 	end
 	if(debugInput:pressed("timestep")) then
 		is_playing=false;
